3452d615f942423561d8374124fa9102df17074c
Yegor Levankov <elevankoff@yandex-team.ru>
2023-05-17 21:40:16 +0300

    Fix

--- Files/boinc_simulator.c
+++ Files/boinc_simulator.c
@@ -777,7 +777,6 @@ int print_results(){
 		for(j=0; j<(int64_t)database->ndata_clients; j++, m++){
 			time_busy += (_dclient_info[m].time_busy);
 			storage += (int64_t)(_dclient_info[m]).total_storage;
-			printf("TOTAL ST: %lld", (int64_t)(_dclient_info[m]).total_storage);
 			tnavailable += _dclient_info[m].navailable;
 		}
 
